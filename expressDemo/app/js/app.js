088c388fb8814aaed517ca1d18c6c6d519309197
lux-02 <oys128950@gmail.com>
2022-02-01 12:09:41 +0000

    ~20 Lecture clear

--- expressDemo/app/js/app.js
+++ expressDemo/app/js/app.js
@@ -1,1 +1,32 @@
-console.log("ㅡㅇ아ㅏ아아아아아ㅡㅇ아아아ㅏ..");
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person 
+// : {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// }
+= {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    // role:[1, 'author']
+    role: Role.ADMIN
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+// person.role = [1, 'admin']
+if (person.role === Role.ADMIN) {
+    console.log('is author');
+}
--- expressDemo/app/js/app.ts
+++ expressDemo/app/js/app.ts
@@ -1,1 +1,32 @@
-console.log("ㅡㅇ아ㅏ아아아아아ㅡㅇ아아아ㅏ..")
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person
+// : {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// }
+    = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    // role:[1, 'author']
+    role: Role.ADMIN
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
+
+// person.role = [1, 'admin']
+
+
+if(person.role === Role.ADMIN){
+    console.log('is author')
+}
